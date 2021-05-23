122492b72caa8b505cbc7846c78bb3c0f01a1bbf
mainak90 <mainak90@gmail.com>
2021-05-23 02:59:47 +0200

    validate manifest annotations

--- vars/validateAnnotations.groovy
+++ vars/validateAnnotations.groovy
@@ -2,9 +2,9 @@
 def call(String filename, String annotation) {
     def fullfilename = "${env.WORKSPACE}/$filename"
     echo "Reading manifest $fullfilename"
+    sh "cat $fullfilename"
     def yamlfile = readYaml (file: fullfilename)
-    def annotations = yamlfile.spec
-    //template.metadata.annotations
+    def annotations = yamlfile.spec.template.metadata.annotations
     echo "$annotations"
     return annotations.containsKey("$annotation")
 }
