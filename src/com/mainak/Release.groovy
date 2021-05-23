0b7162543832e0970b36c6401e1b6368640f6983
mainak90 <mainak90@gmail.com>
2021-05-23 16:29:19 +0200

    validate manifest annotations

--- src/com/mainak/Release.groovy
+++ src/com/mainak/Release.groovy
@@ -86,20 +86,6 @@ class Release implements Serializable {
         }
     }
 
-    def tagRelease(String filepath){
-        def properties = steps.readProperties file: filepath
-        def reponame = properties['reponame']
-        steps.echo "Bumping version set of release"
-        try {
-            steps.sh "bump2version patch --allow-dirty"
-        } catch (Exception ex){
-                steps.echo 'Exception occurred: ' + ex.toString()
-                steps.currentBuild.result = 'FAILURE'
-        }
-        withCredentials([usernamePassword(credentialsId: "mainak90", usernameVariable: "username", passwordVariable: "password")]){
-            sh("git push https://$username:$password@github.com/$reponame.git")
-        }
-    }
 
 
     def deployToK8s(String application, String namespace, boolean Prod){
--- vars/runPRChecks.groovy
+++ vars/runPRChecks.groovy
@@ -1,6 +1,22 @@
 #!/usr/bin/groovy
 import com.mainak.Release
 
+def tagRelease(String filepath){
+    def properties = steps.readProperties file: filepath
+    def reponame = properties['reponame']
+    steps.echo "Bumping version set of release"
+    try {
+        steps.sh "bump2version patch --allow-dirty"
+    } catch (Exception ex){
+            steps.echo 'Exception occurred: ' + ex.toString()
+            steps.currentBuild.result = 'FAILURE'
+    }
+    withCredentials([usernamePassword(credentialsId: "mainak90", usernameVariable: "username", passwordVariable: "password")]){
+        sh("git push https://$username:$password@github.com/$reponame.git")
+    }
+}
+
+
 def call(String filepath){
     pipeline {
         agent {
@@ -30,8 +46,8 @@ def call(String filepath){
             stage('Tag and push') {
                 steps {
                     script {
-                        def rel = new Release(this)
-                        rel.tagRelease("$filepath")
+                        //def rel = new Release(this)
+                        tagRelease("$filepath")
                     }
                 }
             }
